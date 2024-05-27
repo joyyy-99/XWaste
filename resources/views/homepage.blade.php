b6f714d9e0ee4f230f71336ff9f9e8372eb9acd9
kanita04 <kam.anita.04@gmail.com>
2024-05-27 23:11:50 +0300

    Files and folders required for the homepage

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homepage - XWaste</title>
+    <link rel="stylesheet" type="text/css" href="css/homepage.css">
+    <link rel="apple-touch-icon" sizes="180x180" href="favicon_io/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon_io/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon_io/favicon-16x16.png">
+    <link rel="manifest" href="favicon_io/site.webmanifest">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+</head>
+<body>
+    <header class="header">
+        <div class="logo"><a href="#">XWaste</a></div>
+        <nav class="nav">
+            <a href="#home">Home</a>
+            <a href="#about">About Us</a>
+            <a href="#services">Services</a>
+            <a href="#recycling">Recycling</a>
+            <a href="#testimonials">Testimonials</a>
+            <a href="#contact">Contact</a>
+        </nav>
+        <div class="right">
+            <a href="#"><i class="fa-solid fa-right-to-bracket" style="color: #000000;"></i>  Sign in</a>
+        </div>
+    </header>
+    <section class="home" id="home">
+            <div class="welcome-home">
+            <h1>Welcome to XWaste - Revolutionizing Waste Management!</h1>
+            <p>Start managing your household waste efficiently with our platform.
+                Schedule pickups, learn about recycling, and give feedback easily.
+                Let's make our city cleaner and greener together.</p>
+            <a href="#"><button>Sign up</button></a>
+            </div>
+            <div class="img-home">
+            <img src="Images/home.jpg">
+            </div>
+    </section>
+    <section class="about" id="about">
+        <h1>About Us</h1>
+        <div class="about-intro">
+            <p>Our commitment to revolutionizing waste management in Nairobi drives our team at XWaste.             With a deep-rooted passion for sustainability and environmental stewardship, we aim to address the pressing challenges faced by our city.
+                By leveraging technology and community engagement, we strive to create a cleaner, healthier, and more sustainable urban environment for all.
+                Our journey is fueled by a shared vision of fostering responsible waste management practices and promoting a culture of environmental awareness and action.
+                Get to know us better and discover how you can join us in making a positive impact on Nairobi's future.</p>
+        </div>
+        <div class="about-content">
+        <div class="about-text">
+            <h2>Our Mission</h2>
+            <p>We are dedicated to providing effective and efficient waste management solutions to the residents of Nairobi. Our mission is to ensure a cleaner, healthier environment for everyone through innovative technology and community engagement.</p>
+            <h2>Our Vision</h2>
+            <p>To be the leading waste management service provider in Nairobi, setting the standard for environmental responsibility and sustainable practices.</p>
+            <h2>Our Values</h2>
+                <p><i class="fas fa-recycle"></i> Sustainability</p>
+                <p><i class="fas fa-users"></i> Community</p>
+                <p><i class="fas fa-handshake"></i> Integrity</p>
+                <p><i class="fas fa-lightbulb"></i> Innovation</p>
+        </div>
+        <div class="about-img">
+            <img src="Images/about.jpg" alt="About us">
+        </div>
+        </div>
+    </section>
+    <section class="services" id="services">
+    <h1>Our Services</h1>
+        <div class="services-part1">
+            <div class="service1 box">
+                <h2>Waste collection Scheduling</h2>
+                <p>Our platform allows residents to conveniently schedule waste collection
+                    services according to their preferences and needs. Whether it's regular pickups
+                    or special requests, our flexible scheduling system ensures efficient and timely
+                    waste disposal.
+                </p>
+                <a href="#"><button>Schedule a pickup</button></a>
+            </div>
+            <div class="service2 box">
+                <h2>Garbage Bin Request</h2>
+                <p>Need additional garbage bins for your household? Simply submit a request
+                    through our platform, and we will ensure prompt delivery of the required bins
+                    to your doorstep. Proper waste segregation starts with having the right tools,
+                    and we are here to provide them.
+                </p>
+                <a href="#"><button>Request for a bin</button></a>
+            </div>
+        </div>
+        <div class="services-part2">
+            <div class="service3 box">
+                <h2>Feedback and Alerts</h2>
+                <p>We value your feedback! Our platform enables users to share their thoughts,
+                    report issues, or provide suggestions regarding waste management services.
+                    Additionally, stay informed with real-time alerts and updates on collection
+                    schedules and service improvements.
+                </p>
+                <a href="#"><button>Provide Feedback</button></a>
+            </div>
+            <div class="service4 box">
+                <h2>Real-time Tracking</h2>
+                <p>Ever wondered when your garbage will be collected? With our tracking feature,
+                    you can monitor the movement of garbage trucks in your area in real-time. Stay
+                    informed about the status of waste collection and plan your day accordingly.
+                </p>
+            </div>
+        </div>
+    </section>
+    <section class="collection-area">
+        <h1>Where we collect waste in Nairobi</h1>
+        <p>The map provided below shows the areas in Nairobi included
+            in our waste collection route.
+        </p>
+        
+        <div class="map">
+        <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1myIM10iX-7d-FAyvoPMmpdwFlOVq6Pg&ehbc=2E312F&noprof=1" width="640" height="480"></iframe>
+        </div>
+    </section>
+    <section class="recycling" id="recycling">
+        <h1>Recycling</h1>
+        <div class="recycling-box">
+        <div class="img-recycling">
+            <img src="Images/recycling2.jpg" alt="Recycling">
+        </div>
+        <div class="recycling-importance">
+            <h2>Why is recycling important?</h2>
+            <p>Recycling is not just about sorting materials and reducing waste;
+                 it's a vital step towards building a sustainable future for our planet. By recycling, 
+                 we conserve natural resources, reduce pollution, and minimize the carbon footprint 
+                 associated with manufacturing new products. Every bottle, can, or paper that we
+                  recycle contributes to the preservation of ecosystems, the protection of wildlife habitats, 
+                and the mitigation of climate change.</p>
+            <p>At XWaste, we believe in the power of recycling to transform waste into valuable
+                 resources and pave the way towards a cleaner, healthier planet. Through our platform,
+                  we strive to promote recycling awareness, facilitate waste segregation, and empower
+                   individuals to become active participants in the recycling revolution. Together, 
+                   let's embrace the importance of recycling and work towards a more sustainable
+                    future for generations to come.</p>
+        </div>
+        </div>
+        <div class="recyclables">
+            <div class="recyclables-intro">
+            <h2>Recycling guide</h2>
+            <p>Check the guide below to see what you can recycle</p>
+            </div>
+            <div class="recyclables-list">
+            <div class="recyclable1">
+                <h4>Paper and cardboard</h4>
+                <p>Newspapers, magazines, cardboard boxes, office paper</p>
+            </div>
+            <div class="recyclable2">
+                <h4>Plastic Bottles and Containers</h4>
+                <p>Water bottles, soda bottles, yoghurt containers, plastic packaging</p>
+            </div>
+            <div class="recyclable3">
+                <h4>Glass</h4>
+                <p>Glass bottles and jars</p>
+            </div>
+            <div class="recyclable4">
+                <h4>Metal Cans and Foil</h4>
+                <p>Aluminium cans, steel cans, aluminium foil(used for cooking and food storage)</p>
+            </div>
+            </div>
+        </div>
+    </section>
+    <section class="testimonials" id="testimonials">
+    <h1>Testimonials</h1>
+        <div class="ppl-testimonials">
+            <div class="testimonial1">
+                <img src="Images/testimonial1.png" alt="Testimonial 1">
+                <p>"Since using this waste management system, I've noticed a significant
+                     improvement in the cleanliness of our neighborhood. Waste collection is now
+                      prompt and reliable, making it much easier for us to maintain a clean environment."</p>
+                <p><span>- John Kabi, Resident of Nairobi</span></p>
+            </div>
+            <div class="testimonial2">
+                <img src="Images/testimonial2.png" alt="Testimonial 2">
+                <p>"I'm impressed with how this waste management system has empowered residents 
+                    to take control of their waste disposal practices. The ease of scheduling pickups
+                     and the emphasis on recycling and waste segregation have made a significant difference 
+                     in reducing waste pollution in our community." </p>
+                <p><span>- Jane Kimani, Community Leader</span></p>
+            </div>
+            <div class="testimonial3">
+                <img src="Images/testimonial3.png" alt="Testimonial 3">
+                <p>"Thanks to XWaste, managing our waste has become hassle-free. 
+                    The easy scheduling and reliable collection service have made a noticeable difference
+                     in our neighborhood. It's great to see a company committed to both efficiency and environmental responsibility."
+                </p>
+                <p> <span>- Kate Walusala, Resident of Nairobi</span></p>
+            </div>
+        </div>
+    </section>
+    <section class="contact" id="contact">
+        <h1>Contact Us</h1>
+        <div>
+            <p>Feel free to reach out to us through the following contacts:</p><br>
+            <p><i class="fa-solid fa-phone" style="color: #000000;"></i> Phone number: +25487879898 or +254878765776</p><br>
+            <p><i class="fa-solid fa-envelope" style="color: #000000;"></i> Email address: xwaste@gmail.com</p>
+        </div>
+    </section>
+    <footer>
+    <div class="credit"> &copy; copyright @
+      <?php echo date('Y'); ?> All Rights Reserved <br> by <span>Joy Awino & Anita Kamau <br> </span>
+      <span>Terms and Conditions Apply</span>
+    </div>
+    </footer>
+</body>
+</html>
