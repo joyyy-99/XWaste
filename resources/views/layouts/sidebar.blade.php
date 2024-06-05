744f16970b21129bf3f2128dd05872525e7d4598
joyyy-99 <joyawino99@gmail.com>
2024-06-06 01:32:52 +0300

    Fixed Admin Logout button

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -14,10 +14,5 @@
         <li>
             <a href="">Reports</a>
         </li>
-         <!-- Sidebar Toggler (Sidebar) -->
-  <div class="text-center d-none d-md-inline">
-    <button class="rounded-circle border-0" id="sidebarToggle"></button>
-  </div>
-  
     </ul>
 </div>
